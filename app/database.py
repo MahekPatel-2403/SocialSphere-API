0f095f24760d46d85db37390905d34fcf3e63f61
Mahek Patel <mahekharivansh2008@gmail.com>
2026-08-23 23:13:34 +0530

    fix render database connection

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import psycopg
-import time
+
 from .config import settings
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -18,13 +17,3 @@ def get_db():
     finally:
         db.close()
 
-while True:
-    try:
-        conn = psycopg.connect(host = 'localhost', dbname='fastapi', user='postgres', password='postgres123')
-        cursor = conn.cursor()
-        print("Database connection was successfull")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
